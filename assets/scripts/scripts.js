7ebb0a8e2912a45fec9e8c1196fb1c22ef47d873
Svetlana <svetlanatat@mail.ru>
2022-12-06 12:49:27 +0300

    Добавлено модальное окно для кнопки в футере

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,40 @@
+//модальное окно
+function showModalWin() {
+          
+    var darkLayer = document.createElement("div");
+    darkLayer.id = "shadow";
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById("popupWin");
+    modalWin.style.display = "block";
+
+    darkLayer.onclick = function () {  
+    darkLayer.parentNode.removeChild(darkLayer);
+    modalWin.style.display = "none";
+    return false;
+    };
+}
+
+//валидация формы
+
+let form = document.querySelector("#subsribtion")
+let validateBtn = form.querySelector('.window-form-button');
+
+form.addEventListener("submit", (event) => {
+
+    let x = document.querySelector("#name").value;
+    let y = document.querySelector('#email').value;
+
+    if (x == "") {
+      alert("Name must be filled out!");
+    } 
+    else if (y == "") {
+        alert("Email must be filled out!");
+    } else validateBtn.addEventListener('click', function(event) {
+        form.submit(alert('You have subscribed to the news!'));
+    });
+    event.preventDefault()
+});
+
+
+
